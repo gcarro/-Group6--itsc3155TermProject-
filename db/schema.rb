d25491cc6c55d4d7129ab60d80bf7ca5f2843ee2
Devan Nesbit <dnesbit@uncc.edu>
2019-04-08 21:04:09 +0000

    Added description page to view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_202634) do
+ActiveRecord::Schema.define(version: 2019_04_08_201336) do
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
