02d0cb53bf5ac37451e360ed17097aa06097cce5
Bryce Purks <bpurks@uncc.edu>
2019-04-15 21:03:06 +0000

    Added submit and linked to description

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,4 +3,9 @@ class IndexController < ApplicationController
     @foods = Food.all
   end
   
+  def show
+    @foods = Food.find(params[:id])
+    #redirect_to action: :reviews
+  end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+    def index
+        render "description.html.erb"
+        @reviews = Review.all
+        puts params[:foodId]
+        
+        @food = Food.find(params[:foodId])
+        @reviews = Review.where(:ident => params[:foodId])
+    end
+    
+    def new
+        @reviews = Review.new
+    end
+    
+   
 end
